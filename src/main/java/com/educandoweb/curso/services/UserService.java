b169d83388363730772972cb248400951636310c
Camila Brito Pereira <camila-brito10@hotmail.com>
2023-09-07 12:16:42 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -1,6 +1,7 @@
 package com.educandoweb.curso.services;
 
 import java.util.List;
+import java.util.Optional; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,18 @@ import com.educandoweb.curso.entities.User;
 import com.educandoweb.curso.repositories.UserRepository;
 
 @Service
-	public class UserService {
+public class UserService {
 
-		@Autowired
-		private UserRepository repository;
-		
-		public List<User> findAll() {
-			return repository.findAll();
-		}
+    @Autowired
+    private UserRepository repository;
+    
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow();
+    }
 }
+
