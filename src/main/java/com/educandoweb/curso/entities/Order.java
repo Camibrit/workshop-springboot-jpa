5f38e5b2c8cd41b8ad05c7da96233099231d3745
Camila Brito Pereira <camila-brito10@hotmail.com>
2023-09-08 12:54:05 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.curso.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,8 +23,12 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+   
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+    
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -30,12 +37,14 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
-        super();
-        this.id = id;
-        this.moment = moment;
-        this.client = client;
-    }
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
 
     public Long getId() {
         return id;
@@ -52,8 +61,19 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
+    
 
-    public User getClient() {
+    public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
+	public User getClient() {
         return client;
     }
 
--- src/main/java/com/educandoweb/curso/entities/User.java
+++ src/main/java/com/educandoweb/curso/entities/User.java
@@ -27,6 +27,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
    
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
